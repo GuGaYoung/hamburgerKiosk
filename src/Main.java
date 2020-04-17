3211be9a71dd25898b47c7a695e3d60565645284
GuGaYoung <rkdudda12@naver.com>
2020-04-17 22:05:11 +0900

    구입창에 이미지 넣기(햄버거, 디저트 등등)

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,198 @@
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
 
 public class Main {
+	
+	private JFrame frame;
 
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main window = new Main();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	JPanel mainPurchaseScene = new JPanel();
+	JButton[] menuImages = new JButton[9];
+	JButton nextPageButton = new JButton();
+	JButton previousPageButton = new JButton();
+	
+	int menupage = 0;
+	int menuHorizontalLength = 50;
+	int menuVerticalLength = 0;
+	int menuImageWidth = 150;
+	int menuImageHeight = 130;
+	int menuHorizontalInterval = 170;
+	
+	public Main() {
+
+		frame = new JFrame();
+		frame.setBounds(0, 0, 600, 800);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("hamburgerKiosk");
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		frame.getContentPane().setLayout(null);
+		
+		mainPurchaseScene.setBounds(0, 150, 600, 700);
+		frame.getContentPane().add(mainPurchaseScene);
+		mainPurchaseScene.setLayout(null);
+	
+		nextPageButton.setBounds(533, 200, 50, 80);
+		nextPageButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				menupage++;
+				changeMenu();
+			}
+		});
+		mainPurchaseScene.add(nextPageButton);
+		
+		previousPageButton.setBounds(5, 200, 50, 80);
+		previousPageButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				menupage--;
+				changeMenu();
+			}
+		});
+		mainPurchaseScene.add(previousPageButton);
+		
+		for (int i = 0; i < menuImages.length; i++) {
+			mainPurchaseScene.add(menuImages[i] = new JButton());
+			
+			menuImages[i].setHorizontalAlignment(SwingConstants.CENTER);
+			
+			if (i < 3) {
+				menuVerticalLength = 0;
+				menuImages[i].setBounds(menuHorizontalLength, menuVerticalLength, menuImageWidth, menuImageHeight);
+
+			} else if ((i >= 3) && (i < 6)) {
+				menuVerticalLength = 140;
+				menuImages[i].setBounds(menuHorizontalLength, menuVerticalLength, menuImageWidth, menuImageHeight);
+
+			} else if ((i >= 6) && (i < 9)) {
+				menuVerticalLength = 280;
+				menuImages[i].setBounds(menuHorizontalLength, menuVerticalLength, menuImageWidth, menuImageHeight);
+
+			}
+
+			menuHorizontalLength = menuHorizontalInterval + menuHorizontalLength;
+
+			if ((i + 1) % 3 == 0) {
+				menuHorizontalLength = 50;
+			}
+			
+			menuImages[i].setBorderPainted(false);
+			menuImages[i].setContentAreaFilled(false);
+			menuImages[i].setFocusPainted(false);
+		}
+		changeMenu();
+	}
+	
+	public void changeMenu() {
+		//햄버거 세트메뉴
+		if(menupage == 0) {
+			menuImages[0].setIcon(new ImageIcon("./hamburgerSetimages/더블x2세트.png"));
+			menuImages[1].setIcon(new ImageIcon("./hamburgerSetimages/와규에디션2세트.png"));
+			menuImages[2].setIcon(new ImageIcon("./hamburgerSetimages/AZ버거세트.png"));
+			menuImages[3].setIcon(new ImageIcon("./hamburgerSetimages/한우불고기콤보.png"));
+			menuImages[4].setIcon(new ImageIcon("./hamburgerSetimages/한우불고기세트.png"));
+			menuImages[5].setIcon(new ImageIcon("./hamburgerSetimages/모짜렐라인더버거베이컨세트.png"));
+			menuImages[6].setIcon(new ImageIcon("./hamburgerSetimages/원조빅불세트.png"));
+			menuImages[7].setIcon(new ImageIcon("./hamburgerSetimages/핫크리스피세트.png"));
+			menuImages[8].setIcon(new ImageIcon("./hamburgerSetimages/불고기버거세트.png"));
+		}
+		
+		else if(menupage == 1) {
+			menuImages[0].setIcon(new ImageIcon("./hamburgerSetimages/리아미라클버거세트.png"));
+			menuImages[1].setIcon(new ImageIcon("./hamburgerSetimages/새우버거세트.png"));
+			menuImages[2].setIcon(new ImageIcon("./hamburgerSetimages/치킨버거세트.png"));
+			menuImages[3].setIcon(new ImageIcon("./hamburgerSetimages/T-Rax버거세트.png"));
+			menuImages[4].setIcon(new ImageIcon("./hamburgerSetimages/클래식치즈버거세트.png"));
+			menuImages[5].setIcon(new ImageIcon("./hamburgerSetimages/데리버거세트.png"));
+			menuImages[6].setIcon(new ImageIcon());
+			menuImages[7].setIcon(new ImageIcon());
+			menuImages[8].setIcon(new ImageIcon());
+		}
+		
+		//햄버거
+		else if(menupage == 2) {
+			menuImages[0].setIcon(new ImageIcon("./hamburgerimages/와규에디션2.png"));
+			menuImages[1].setIcon(new ImageIcon("./hamburgerimages/더블x2.png"));
+			menuImages[2].setIcon(new ImageIcon("./hamburgerimages/모짜렐라인더버거베이컨.png"));
+			menuImages[3].setIcon(new ImageIcon("./hamburgerimages/AZ버거.png"));
+			menuImages[4].setIcon(new ImageIcon("./hamburgerimages/원조빅불.png"));
+			menuImages[5].setIcon(new ImageIcon("./hamburgerimages/핫크리스피버거.png"));
+			menuImages[6].setIcon(new ImageIcon("./hamburgerimages/한우불고기.png"));
+			menuImages[7].setIcon(new ImageIcon("./hamburgerimages/리아미라클버거.png"));
+			menuImages[8].setIcon(new ImageIcon("./hamburgerimages/새우버거.png"));
+		}
+		
+		else if(menupage == 3) {
+			menuImages[0].setIcon(new ImageIcon("./hamburgerimages/치킨버거.png"));
+			menuImages[1].setIcon(new ImageIcon("./hamburgerimages/T-Rax.png"));
+			menuImages[2].setIcon(new ImageIcon("./hamburgerimages/클래식치즈버거.png"));
+			menuImages[3].setIcon(new ImageIcon("./hamburgerimages/데리버거.png"));
+			menuImages[4].setIcon(new ImageIcon());
+			menuImages[5].setIcon(new ImageIcon());
+			menuImages[6].setIcon(new ImageIcon());
+			menuImages[7].setIcon(new ImageIcon());
+			menuImages[8].setIcon(new ImageIcon());
+		}
+		
+		//디저트
+		else if(menupage == 4) {
+			menuImages[0].setIcon(new ImageIcon("./dessertimages/치즈인더에그.png"));
+			menuImages[1].setIcon(new ImageIcon("./dessertimages/지파이하비네로.png"));
+			menuImages[2].setIcon(new ImageIcon("./dessertimages/지파이고소한맛.png"));
+			menuImages[3].setIcon(new ImageIcon("./dessertimages/치킨너겟.png"));
+			menuImages[4].setIcon(new ImageIcon("./dessertimages/롱치즈스틱.png"));
+			menuImages[5].setIcon(new ImageIcon("./dessertimages/콘샐러드.png"));
+			menuImages[6].setIcon(new ImageIcon("./dessertimages/선데아이스크림.png"));
+			menuImages[7].setIcon(new ImageIcon("./dessertimages/쉑쉑치킨.png"));
+			menuImages[8].setIcon(new ImageIcon("./dessertimages/양념감자.png"));
+		}
+		
+		else if(menupage == 5) {
+			menuImages[0].setIcon(new ImageIcon("./dessertimages/오징어링.png"));
+			menuImages[1].setIcon(new ImageIcon("./dessertimages/치즈스틱.png"));
+			menuImages[2].setIcon(new ImageIcon("./dessertimages/포테이토.png"));
+			menuImages[3].setIcon(new ImageIcon("./dessertimages/토네이도(초코쿠키).png"));
+			menuImages[4].setIcon(new ImageIcon("./dessertimages/토네이도(녹차).png"));
+			menuImages[5].setIcon(new ImageIcon("./dessertimages/토네이도(스트로베리).png"));
+			menuImages[6].setIcon(new ImageIcon("./dessertimages/소프트콘.png"));
+			menuImages[7].setIcon(new ImageIcon());
+			menuImages[8].setIcon(new ImageIcon());
+		}
+		
+		//드링크
+		else if(menupage == 6) {
+			menuImages[0].setIcon(new ImageIcon("./drinkimages/아이스아메리카노.png"));
+			menuImages[1].setIcon(new ImageIcon("./drinkimages/사이다.png"));
+			menuImages[2].setIcon(new ImageIcon("./drinkimages/콜라.png"));
+			menuImages[3].setIcon(new ImageIcon("./drinkimages/아메리카노.png"));
+			menuImages[4].setIcon(new ImageIcon("./drinkimages/카페라떼.png"));
+			menuImages[5].setIcon(new ImageIcon("./drinkimages/아이스카페라떼.png"));
+			menuImages[6].setIcon(new ImageIcon());
+			menuImages[7].setIcon(new ImageIcon());
+			menuImages[8].setIcon(new ImageIcon());
+		}
+	}
 }
